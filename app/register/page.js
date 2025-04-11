555d8f6adc48deeec1d4496b1979428f1daf4966
shalu1529 <144725677+shalu1529@users.noreply.github.com>
2025-04-11 18:28:26 +0530

    Update page.js

--- app/register/page.js
+++ app/register/page.js
@@ -13,7 +13,7 @@ export default function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('http://localhost:5000/api/auth/register', {
+      const res = await fetch('https://blog-co8w.onrender.com/api/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
