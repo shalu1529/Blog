2a95fee246f7c1dd9b7a58b6010880fbdb5bf81c
shalu1529 <144725677+shalu1529@users.noreply.github.com>
2025-04-11 18:25:16 +0530

    Update page.js url

--- app/createPost/page.js
+++ app/createPost/page.js
@@ -98,7 +98,7 @@ const CreatePost = () => {
     const token = localStorage.getItem('token');
 
     try {
-      const response = await fetch('http://localhost:5000/api/posts', {
+      const response = await fetch('https://blog-co8w.onrender.com/api/posts', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
